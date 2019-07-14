5be29ca867107aa6b44585aea377e8359848bc28
George Caras <george@MacBook-Pro.fios-router.home>
2019-07-14 11:12:23 -0700

    Added game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,15 @@
 console.log("Up and Running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped"+cardOne);
-console.log("User flipped"+ cardThree);
+var cards = ["queen", "queen", "king", "king"]
+var cardInPlay = []
+var cardOne = cards[0]
+cardInPlay.push("cardOne")
+console.log("User flipped" + " " + cardOne);
+var cardTwo = cards[2]
+cardInPlay.push("cardTwo")
+console.log("User flipped" + " " + cardTwo);
+if (cardInPlay.length === 2) {
+	} if (cardInPlay[0] === cardInPlay[1]) {
+		alert("You found a match!");
+	} else if (cardInPlay [0] !== cardInPlay[1]) {
+		alert("Sorry, try again.");
+	}
